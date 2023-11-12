5be3bcb802300c0cd9ed19c055029d950a16be64
Vahan Hovhannisyan <vaa.hovanisyan@gmail.com>
2023-11-12 10:29:55 +0400

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,137 @@
+import gulp from "gulp";
+import del from "del";
+import gulpFileInclude from "gulp-file-include";
+import gulpTypograf from "gulp-typograf";
+import gulpGroupCssMediaQueries from "gulp-group-css-media-queries";
+import gulpNotify from "gulp-notify";
+import gulpPlumber from "gulp-plumber";
+import browserSync from "browser-sync";
+import fs from "fs";
+import gulpSourcemaps from "gulp-sourcemaps";
+import imagemin from "gulp-imagemin";
+import gulpSvgSprite from "gulp-svg-sprite";
+import gulpAutoprefixer from "gulp-autoprefixer";
+import gulpSass from "gulp-sass";
+import * as sass from 'sass';
+const dartSass = gulpSass(sass);
+const srcFolder = './src/';
+const destFolder = './public/';
+
+
+const html = () => {
+  return gulp.src(`${srcFolder}*.html`)
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError(error => ({
+        title: "html",
+        message: error.message
+      }))
+    }))
+    .pipe(gulpFileInclude({
+      prefix: '@',
+      basepath: '@file'
+    }))
+    .pipe(gulpTypograf({
+      locale: ['ru', 'en-US']
+    }))
+    .pipe(gulp.dest(destFolder))
+}
+export { html }
+
+const css = () => {
+  return gulp.src(`${srcFolder}*.scss`)
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError(error => ({
+        title: "css",
+        message: error.message
+      }))
+    }))
+    .pipe(dartSass())
+    // .pipe(gulpGroupCssMediaQueries())
+    .pipe(gulpAutoprefixer({
+      cascade: false,
+      grid: true,
+      overrideBrowserslist: ["last 5 versions"]
+    }))
+
+    .pipe(gulp.dest(`${destFolder}css/`))
+}
+export { css }
+
+const js = () => {
+  return gulp.src(`${srcFolder}*.js`)
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError(error => ({
+        title: "js",
+        message: error.message
+      }))
+    }))
+
+    .pipe(gulp.dest(`${destFolder}js/`))
+}
+export { js }
+
+const img = () => {
+  return gulp.src(`${srcFolder}img/**/*.{jpeg,jpg,png,gif,ico,webp,webmanifest,xml,json}`)
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError(error => ({
+        title: "img",
+        message: error.message
+      }))
+    }))
+    .pipe(imagemin({ verbose: true }))
+    .pipe(gulp.dest(`${destFolder}img/`))
+}
+export { img }
+
+const svg = () => {
+  return gulp.src(`${srcFolder}svg/**/*.svg`)
+    .pipe(gulpPlumber({
+      errorHandler: gulpNotify.onError(error => ({
+        title: "svg",
+        message: error.message
+      }))
+    }))
+    .pipe(gulpSvgSprite({
+      mode: {
+        stack: {
+          sprite: `../sprite.svg`,
+        },
+      }
+    }))
+    .pipe(gulp.dest(`${destFolder}img/svg`))
+}
+export { svg }
+
+const server = () => {
+  browserSync.init({
+    server: {
+      baseDir: destFolder
+    }
+  })
+}
+export { server }
+
+const clean = () => {
+  return del(destFolder)
+}
+export { clean }
+
+const watcher = () => {
+  // gulp.watch(path.watch.files, copy).on("all", browserSync.reload)
+  gulp.watch(`${srcFolder}**/*.html`, html).on("all", browserSync.reload)
+  gulp.watch(`${srcFolder}**/*.scss`, css).on("all", browserSync.reload)
+  gulp.watch(`${srcFolder}**/*.js`, js).on("all", browserSync.reload)
+  gulp.watch(`${srcFolder}**/*.{jpeg,jpg,png,gif,ico,webp,webmanifest,xml,json}`, img).on("all", browserSync.reload)
+  gulp.watch(`${srcFolder}**/*.svg`, svg).on("all", browserSync.reload)
+  // gulp.watch(path.watch.fonts, fonts).on("all", browserSync.reload)
+}
+
+const mainTasks = gulp.parallel(html, css, js, img, svg)
+
+// npm run dev
+const dev = gulp.series(clean, mainTasks, gulp.parallel(server, watcher))
+export { dev }
+
+// npm run build
+const build = gulp.series(clean, mainTasks)
+export { build }
